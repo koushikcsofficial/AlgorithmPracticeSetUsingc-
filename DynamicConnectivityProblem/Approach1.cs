edaf219a8b78e30ff64994296ccc6e7d52951b45
KOUSHIK SAHA <koushik.official1999@gmail.com>
2020-08-13 20:30:26 +0530

    Add project files.

---
+++ DynamicConnectivityProblem/Approach1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicConnectivityProblem
+{
+    public class Approach1
+    {
+        int[] arr;
+        public Approach1(int lengthOftheArray)
+        {
+            int[] arr = new int[lengthOftheArray];
+            //initializing the values in the array
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = i;
+        }
+        //method to create a route
+        public void Connected(int fromPoint, int toPoint)
+        {
+            if (IsValidNode(fromPoint) && IsValidNode(toPoint))
+            {
+                //Initializing the same value to both of the points
+                if (CountThePresenceofElement(fromPoint) > 1)
+                {
+                    //Whenever it will encounter fromPoint in the i'th index of the arr
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (arr[i] == fromPoint)
+                        {
+                            //Replace the fromPoint with toPoint
+                            arr[i] = toPoint;
+                        }
+                    }
+
+                }
+                else
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (arr[i] == toPoint)
+                        {
+                            arr[i] = fromPoint;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have tried to create a path between two invalid node");
+            }
+        }
+
+        //Find two points if they are connected
+        public void Find(int fromPoint, int toPoint)
+        {
+            if (IsValidNode(fromPoint) && IsValidNode(toPoint))
+            {
+                if (arr[fromPoint] == arr[toPoint])
+                {
+                    Console.WriteLine("A connection found between two nodes you entered");
+                }
+                else
+                {
+                    Console.WriteLine("There is no such connection found between the nodes you entered");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You are trying to find a route between two invalid node");
+            }
+        }
+        //Checking if a node is valid
+        public bool IsValidNode(int node)
+        {
+            if (node <= arr.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Count how many times a value is present in the array
+        public int CountThePresenceofElement(int element)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == element)
+                {
+                    count = count + 1;
+                }
+                else
+                {
+                    count = count + 0;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ DynamicConnectivityProblem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicConnectivityProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of nodes:");
+            int points = 0;
+            points = Convert.ToInt32(Console.ReadLine());
+
+            Approach1 ap1 = new Approach1(points);
+
+            //Create route here
+            ap1.Connected(3, 4);
+            ap1.Connected(3, 8);
+            ap1.Connected(1, 5);
+            ap1.Connected(1, 2);
+
+            //Find route here
+            ap1.Find(5, 4);
+            ap1.Find(8, 4);
+
+            //Freeze the console
+            Console.ReadLine();
+        }
+    }
+}
